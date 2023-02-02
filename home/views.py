7ab98bbbb5328ceb693068196c7eef979e8f652c
Vlad <vlad.bashtannyk11@gmail.com>
2023-02-02 17:52:27 +0200

    Implemented sign up page

--- home/views.py
+++ home/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = "home/signup_page.html"
+    success_url = "/service"
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect("/admin")
+        return super().get(request, *args, **kwargs)
+

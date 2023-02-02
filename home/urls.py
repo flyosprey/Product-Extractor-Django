352bd9d20bb8bf5f1b2d758d4a76df43ba60fc10
Vlad <vlad.bashtannyk11@gmail.com>
2023-02-02 18:21:52 +0200

    Small fixes

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from views import SignupView, LoginInterfaceView, LogoutInterfaceView, HomeView
+from .views import SignupView, LoginInterfaceView, LogoutInterfaceView, HomeView
 
 
 urlpatterns = [
